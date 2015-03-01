3b0b2f9fdde85294ae2df95653a97d11c0279a95
Jason Kriss <jasonkriss@gmail.com>
2015-03-01 14:56:28 -0800

    move session code into concern for easier overrideing

---
+++ app/models/concerns/pollett/sessionable.rb
@@ -0,0 +1,36 @@
+module Pollett
+  module Sessionable
+    extend ActiveSupport::Concern
+
+    included do
+      belongs_to :user, class_name: Pollett.config.user_model_name
+
+      before_create :set_token
+
+      scope :active, -> { where('accessed_at >= ? AND revoked_at IS NULL', Pollett.config.timeout.ago) }
+    end
+
+    module ClassMethods
+      def authenticate(token)
+        active.find_by(token: token)
+      end
+    end
+
+    def access(request)
+      update({
+        accessed_at: current_time_from_proper_timezone,
+        ip: request.remote_ip,
+        user_agent: request.user_agent
+      })
+    end
+
+    def revoke!
+      update!(revoked_at: current_time_from_proper_timezone)
+    end
+
+    private
+    def set_token
+      self.token = Pollett.generate_token
+    end
+  end
+end
