29f3d6dd5133cf730849600bea58d7e6ddbb8e28
Jason Kriss <jasonkriss@gmail.com>
2015-03-23 10:19:47 -0700

    turn off format, and turn on shallow nesting for all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Pollett::Engine.routes.draw do
-  resources :sessions, only: [:create] do
-    collection do
-      get "current", action: :show
-      delete "current", action: :destroy
-      post "forgot"
+  scope shallow: true, format: false do
+    resources :sessions, only: [:create] do
+      collection do
+        get "current", action: :show
+        delete "current", action: :destroy
+        post "forgot"
+      end
     end
   end
 end
